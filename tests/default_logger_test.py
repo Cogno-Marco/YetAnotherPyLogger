ca6ecfb86997da00992a79a78a21864596cf931a
Cogno-Marco <marco.cognolato.98@gmail.com>
2020-10-02 22:18:11 +0200

    added timestamp format and change tests

--- tests/default_logger_test.py
+++ tests/default_logger_test.py
@@ -15,10 +15,12 @@ class TestLogMethods(unittest.TestCase):
     
     BOLD = "\33[1m"
     TEXT = "working"
+    DEFAULT_TIME_FORMAT = "[%Y-%m-%d %H:%M:%S]"
     
     def setUp(self) -> None:
         log.disable_timestamp()
         log.disable_save_to_txt()
+        log.set_log_timestamp_format(self.DEFAULT_TIME_FORMAT)
     
     def test_info_as_expected(self):
         expected_log = f"{self.RESET}{self.BLUE}{self.BOLD}[?] Info: {self.RESET}{self.TEXT}{self.RESET}\n"
@@ -91,6 +93,60 @@ class TestLogMethods(unittest.TestCase):
         with patch('sys.stdout', new = StringIO()) as fake_out: 
             log.success(self.TEXT)
             self.assertEqual(fake_out.getvalue(), expected_log)
+    
+    def test_format_change_info(self):
+        log.enable_timestamp()
+        time_format = "[%H:%M:%S]"
+        log.set_log_timestamp_format(time_format)
+        
+        time = datetime.now()
+        time_formatted = time.strftime(time_format)
+        
+        expected_log = f"{self.RESET}{self.GREEN}{self.BOLD}{time_formatted}[+] Success: {self.RESET}{self.TEXT}{self.RESET}\n"
+        with patch('sys.stdout', new = StringIO()) as fake_out: 
+            log.success(self.TEXT)
+            self.assertEqual(fake_out.getvalue(), expected_log)
             
+    
+    def test_format_change_warning(self):
+        log.enable_timestamp()
+        time_format = "[%H:%M:%S]"
+        log.set_log_timestamp_format(time_format)
+        
+        time = datetime.now()
+        time_formatted = time.strftime(time_format)
+        
+        expected_log = f"{self.RESET}{self.YELLOW}{self.BOLD}{time_formatted}[!] Warning: {self.RESET}{self.TEXT}{self.RESET}\n"
+        with patch('sys.stdout', new = StringIO()) as fake_out: 
+            log.warning(self.TEXT)
+            self.assertEqual(fake_out.getvalue(), expected_log)
+    
+    def test_format_change_error(self):
+        log.enable_timestamp()
+        time_format = "[%H:%M:%S]"
+        log.set_log_timestamp_format(time_format)
+        
+        time = datetime.now()
+        time_formatted = time.strftime(time_format)
+        
+        expected_log = f"{self.RESET}{self.RED}{self.BOLD}{time_formatted}[-] Error: {self.RESET}{self.TEXT}{self.RESET}\n"
+        with patch('sys.stdout', new = StringIO()) as fake_out: 
+            log.error(self.TEXT)
+            self.assertEqual(fake_out.getvalue(), expected_log)
+    
+    def test_format_change_success(self):
+        log.enable_timestamp()
+        time_format = "[%H:%M:%S]"
+        log.set_log_timestamp_format(time_format)
+        
+        time = datetime.now()
+        time_formatted = time.strftime(time_format)
+        
+        expected_log = f"{self.RESET}{self.GREEN}{self.BOLD}{time_formatted}[+] Success: {self.RESET}{self.TEXT}{self.RESET}\n"
+        with patch('sys.stdout', new = StringIO()) as fake_out: 
+            log.success(self.TEXT)
+            self.assertEqual(fake_out.getvalue(), expected_log)
+    
+     
 if __name__ == '__main__':
     unittest.main()
